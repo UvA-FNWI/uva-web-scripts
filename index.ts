5a34b69f69704cbb9699e282e30d756707e712cf
Jeremy Barisch Rooney <barischrooneyj@gmail.com>
2019-08-29 21:37:21 +0200

    Update idnex.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,2 @@
 import * as UvA from './index-exports';
 export { UvA };
-//class UvA {
-
-//  public static Click: Click = Click;
-//  public static DataNose: DataNose = DataNose;
-
-//}
-
-//declare global {
-//    interface Window { Click: Click; }
-//}
-
-//window.Click = window.Click || {};
-
-//export { UvA };
